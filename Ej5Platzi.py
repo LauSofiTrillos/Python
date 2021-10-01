d9a83766166984cb01b286fea1d04d0c96bfb0e3
Laura Sofia Hernandez Trillos <88508324+LauSofiTrillos@users.noreply.github.com>
2021-09-30 23:04:51 -0500

    Create Ej5Platzi.py

---
+++ Ej5Platzi.py
@@ -0,0 +1,29 @@
+# Conversor de monedas utilizando una funcion 
+
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("Cuantos pesos " + tipo_pesos + " tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos/valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + "dolares")
+
+menu = """
+Bienvenido al Conversor de Monedas 💰
+
+1 - Pesos colombianos
+2 - Pesos argentinos 
+3 - Pesos mexicanos
+
+Elige una opcion:  """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("colombianos", 3875)
+elif opcion == 2:
+    conversor("argentinos", 65)
+elif opcion == 3:
+    conversor("mexicanos", 24)
+else:
+    print("Ingrese una opcion correcta")
