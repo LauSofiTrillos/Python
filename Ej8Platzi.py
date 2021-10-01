8542ad9f533245465fde63a980567852ab4e6c6f
Laura Sofia Hernandez Trillos <88508324+LauSofiTrillos@users.noreply.github.com>
2021-10-01 15:23:10 -0500

    Create Ej8Platzi.py

---
+++ Ej8Platzi.py
@@ -0,0 +1,15 @@
+# bucle for 
+
+# numeros del 0 al 999
+a = list(range(1000))
+print(a)
+
+
+# numeros del 1 al 1000
+for contador in range(1,1001):
+    print(contador)
+
+
+# tabla del 11:  
+for i in range(10):
+    print(11*i)
