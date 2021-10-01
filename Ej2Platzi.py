303b7a65d9a66d86b4ae7ef11bf5cb810c67b52c
Laura Sofia Hernandez Trillos <88508324+LauSofiTrillos@users.noreply.github.com>
2021-09-30 19:53:16 -0500

    Create Ej2Platzi.py

---
+++ Ej2Platzi.py
@@ -0,0 +1,17 @@
+# Condicionales
+
+# IF
+edad = int(input("Escribe tu Edad: "))
+if edad > 17:
+    print("Eres Mayor de Edad")
+else:
+    print("Eres menor de Edad")
+
+# ELSE IF     
+numero = int(input("Escribe un numero: ")
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else: 
+    print("Es menor a 5")    
